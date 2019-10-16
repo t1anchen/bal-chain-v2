1b8735a68dbfb40089844a97f5cdeac53b9cd3ef
au9ustine <duke.augustine@gmail.com>
2019-10-17 00:26:52 +0800

    add donation

--- donation.sol
+++ donation.sol
@@ -1,31 +1,55 @@
-pragma solidity ^0.4.10;
+pragma solidity ^0.4.24;
 
 contract Donation {
 
     uint amount;
-    string unit;
+    uint last_created;
+    bytes32 tx_id;
+    uint service_fee; // ufix type cannot be assigned, use uint instead
+    uint evaluation_fee; // ufix type cannot be assigned, use uint instead
+    Donator donator;
+    Project project;
+
+    bytes32 evidence;
+
+    mapping(address => Project) projects;
     mapping(address => Donator) donators;
 
+    struct Project {
+        address funding;
+        bool is_active;
+        Donation[] pending;
+        uint target;
+    }
 
     struct Donator {
-        address id;
-        string name;
-        uint factor;
+        address account;
+        uint karma;
     }
 
-    struct ServiceProvider {
-        address id;
-        string name;
-        uint factor;
+    constructor(uint amount_from_donator, address donator_addr, address proj_addr) public {
+        amount = amount_from_donator;
+        last_created = now;
+        tx_id = keccak256(donator_addr, last_created, amount_from_donator);
+        service_fee = amount_from_donator * 10 / 9;
+        evaluation_fee = amount_from_donator * 100 / 5;
+        donator = donators[donator_addr];
+        project = projects[proj_addr];
     }
 
-    constructor(address donotor, uint amount) {
-        amount = amount;
-        unit = "RMB";
-        donators[donotor].factor += amount;
+    function donate() public {
+        if (project.is_active) {
+            if (donator.account.balance > amount) {
+                project.funding.transfer(amount);
+                donator.karma += amount;
+            }
+        }
+
+        if (project.funding.balance >= project.target) {
+            project.is_active = false;
+        }
+
+        project.pending.push(this);
     }
-
-    
-
 
 }
